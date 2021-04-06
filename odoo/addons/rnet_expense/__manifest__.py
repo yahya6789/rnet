33b04690f81a3b84ba92cebc6a4217e82089c40e
Yahya <yahya6789@gmail.com>
2021-04-06 12:29:58 +0700

    Initial commit

---
+++ odoo/addons/rnet_expense/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "RNET - Expenses",
+    'author': "RNET",
+    'version': '0.1',
+
+    'depends': ['hr_expense'],
+
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
