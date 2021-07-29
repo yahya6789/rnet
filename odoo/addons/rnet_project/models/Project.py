56b5830e0e365709c02eab81b148d4c6a4c3455e
Yahya <yahya6789@gmail.com>
2021-07-29 14:51:27 +0700

    Tambah field: Team member

--- odoo/addons/rnet_project/models/Project.py
+++ odoo/addons/rnet_project/models/Project.py
@@ -41,6 +41,11 @@ class Project(models.Model):
     actual_delivery_date = fields.Date(string='Actual Delivery Date')
     term_of_delivery = fields.Char(string='Term of Delivery')
     notes = fields.Text(string='Notes')
+    project_manager = fields.Many2one('res.users', string='Project Manager', required=True)
+    project_coordinator = fields.Many2one('res.users', string='Project Coordinator')
+    pic_technical = fields.Many2one('res.users', string='PIC Technical')
+    pic_project_cost = fields.Many2one('res.users', string='PIC Project Cost')
+    team_member = fields.Many2many('res.users', string='Team Member')
 
     @api.model
     def create(self, vals):
