ec4485a1bdf4bbd89147685949653a6f1570e378
Yahya <yahya6789@gmail.com>
2021-08-04 10:49:58 +0700

    RNET Inventory

---
+++ odoo/addons/rnet_inventory/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "RNET - Inventory",
+    'author': "RNET",
+    'version': '0.1',
+
+    'depends': ['stock'],
+
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
