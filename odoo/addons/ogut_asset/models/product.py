4bf06de2689457446b967e104c0f9b8ddd044567
YAHYA\Yahya <yahya6789@gmail.com>
2021-09-21 11:20:36 +0700

    Task 431

--- odoo/addons/ogut_asset/models/product.py
+++ odoo/addons/ogut_asset/models/product.py
@@ -1,22 +1,28 @@
 from odoo import models, fields, api, _
 from datetime import date
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     po_generate_multiple_asset = fields.Boolean('PO Generate Multiple Asset')
+    product_prefix = fields.Char("Product Prefix")
+
 
 class Product(models.Model):
     _inherit = 'product.product'
 
-    #categ_id
-    is_generate_multiple_asset = fields.Boolean(compute='_is_generate_multiple_asset', string='Is Generate Multiple Asset')
+    # categ_id
+    is_generate_multiple_asset = fields.Boolean(compute='_is_generate_multiple_asset',
+                                                string='Is Generate Multiple Asset')
 
     @api.one
     def _is_generate_multiple_asset(self):
         self.is_generate_multiple_asset = self.categ_id.po_generate_multiple_asset
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
