f1ca5fe02453b8cd8fb45023894d7c721060c4d2
YAHYA\Yahya <yahya6789@gmail.com>
2021-09-21 16:26:38 +0700

    Task 436

--- odoo/addons/rnet_purchase_requisition/models/PurchaseRequisitionLine.py
+++ odoo/addons/rnet_purchase_requisition/models/PurchaseRequisitionLine.py
@@ -10,5 +10,13 @@ _logger = logging.getLogger(__name__)
 class PurchaseRequisitionLine(models.Model):
     _inherit = 'material.purchase.requisition.line'
 
-    brand = fields.Char ('Brand')
-    remark = fields.Char ('Remark')
+    brand = fields.Many2one('gut.brand', 'Brand')
+    brand_note = fields.Char('Brand Note')
+    remark = fields.Char('Remark')
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        for rec in self:
+            rec.description = rec.product_id.name
+            rec.uom = rec.product_id.uom_id.id
+            rec.brand = rec.product_id.brand.id
