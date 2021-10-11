1908121c1e2510d4846d0d13e98ee7fe80415099
YAHYA\Yahya <yahya6789@gmail.com>
2021-10-11 11:29:01 +0700

    Bugfix #b31cf01

---
+++ odoo/addons/rnet_purchase_requisition/models/PurchaseRequisitionLine.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api, _
+from datetime import datetime, date
+from odoo.exceptions import Warning, UserError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseRequisitionLine(models.Model):
+    _inherit = 'material.purchase.requisition.line'
+
+    brand = fields.Many2one('gut.brand', 'Brand')
+    brand_note = fields.Char('Brand Note')
+    remark = fields.Char('Remark')
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        for rec in self:
+            rec.description = rec.product_id.name
+            rec.uom = rec.product_id.uom_id.id
+            rec.brand = rec.product_id.brand.id
