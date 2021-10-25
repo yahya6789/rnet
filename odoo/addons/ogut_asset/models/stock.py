9e0aa94ba3a36dfc66fa6e2efde1d9aab32e8ef7
YAHYA\Yahya <yahya6789@gmail.com>
2021-10-25 16:33:36 +0700

    Task 98.

--- odoo/addons/ogut_asset/models/stock.py
+++ odoo/addons/ogut_asset/models/stock.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, tools, exceptions, _
-from odoo.exceptions import ValidationError
+import datetime
 
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    #category id 38 = Inventory
+    # category id 38 = Inventory
     group_access = fields.Many2one('res.groups', string="Group Access", domain="[('category_id', '=', 38)]")
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.one
+    @api.depends('move_lines.date_expected')
+    def _compute_scheduled_date(self):
+        po = self.env['purchase.order'].search([
+            ('name', '=', self.origin)
+        ], limit=1)
+        self.scheduled_date = po.date_planned  # datetime.datetime(1970, 12, 10)
