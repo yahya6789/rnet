5e01f19a0b704988396e9d07f75c9414589b1947
YAHYA\Yahya <yahya6789@gmail.com>
2021-11-01 14:37:51 +0700

    Task 110.

--- odoo/addons/ogut_asset/wizard/purchase_requisition_reject_wizard.py
+++ odoo/addons/ogut_asset/wizard/purchase_requisition_reject_wizard.py
@@ -11,7 +11,7 @@ class PurchaseRequisitionRejectWizard(models.TransientModel):
         pr = self.env['material.purchase.requisition'].browse(self._context['requisition_id'])
         pr.write({
             'state': 'reject',
-            'reject_employee_id': self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1),
+            'reject_employee_id': self.env['hr.employee'].sudo().search([('user_id', '=', self.env.uid)], limit=1).id,
             'userreject_date': fields.Date.today(),
             'reject_reason': self.reject_reason,
         })
