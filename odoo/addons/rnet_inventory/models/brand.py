57eb891eb7e83b94bcb6a290824aec9beb50b95b
YAHYA\Yahya <yahya6789@gmail.com>
2021-11-24 15:57:01 +0700

    Task 158.

--- odoo/addons/rnet_inventory/models/brand.py
+++ odoo/addons/rnet_inventory/models/brand.py
@@ -5,10 +5,30 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class brand(models.Model):
+class Brand(models.Model):
     _name = 'gut.brand'
     _description = 'Product Brand'
 
     code = fields.Char('Brand Type', required=True)
     name = fields.Char('Brand', required=True)
     alias = fields.Char('Alias')
+
+    product_count = fields.Integer(string='Product Count', compute='_gut_product_count')
+
+    @api.multi
+    def open_products(self):
+        for brand in self:
+            return {
+                'name': 'Products',
+                'view_type': 'form',
+                'view_mode': 'tree,form',
+                'res_model': 'product.template',
+                'type': 'ir.actions.act_window',
+                'domain': [('brand', '=', brand.id)],
+            }
+        pass
+
+    @api.multi
+    def _gut_product_count(self):
+        res = self.env['product.template'].search_count([('brand', '=', self.id)])
+        self.product_count = res or 0
--- odoo/addons/rnet_inventory/models/product_group.py
+++ odoo/addons/rnet_inventory/models/product_group.py
@@ -6,10 +6,11 @@ class ProductTemplateInherit(models.Model):
 
     product_group = fields.Many2one('product.group', string='Product Group')
 
+
 class ProductGroup(models.Model):
     _name = "product.group"
 
-    _sql_constraints = [('code_uniq', 'unique (code)', "Product group code must be unique!"),]
+    _sql_constraints = [('code_uniq', 'unique (code)', "Product group code must be unique!"), ]
 
     code = fields.Char('Code', required=True)
     name = fields.Char('Name')
